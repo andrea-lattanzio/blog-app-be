f45725af630458bbe8d933b8d5c8aa36795c3eef
Ltensio <76816528+Ltensio@users.noreply.github.com>
2025-03-03 23:30:55 +0100

    feat(article): add file structure for crud operations on article entity

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,10 +3,12 @@ import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { ConfigModule, ConfigService } from '@nestjs/config';
 import { join } from 'path';
+import { ArticleModule } from './article/article.module';
 
 @Module({
   imports: [
     ConfigModule.forRoot({ isGlobal: true, envFilePath: getEnvPath() }),
+    ArticleModule,
   ],
   controllers: [AppController],
   providers: [AppService],
