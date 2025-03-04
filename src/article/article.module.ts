4c6957697b43cd0b1c7b5df58bb52edaccd7ff82
Ltensio <76816528+Ltensio@users.noreply.github.com>
2025-03-04 12:21:21 +0100

    feat(article-crud): add service methods for crud operations on article entity

--- src/article/article.module.ts
+++ src/article/article.module.ts
@@ -1,9 +1,10 @@
 import { Module } from '@nestjs/common';
 import { ArticleService } from './article.service';
 import { ArticleController } from './article.controller';
+import { DatabaseService } from 'src/config/database/database.service';
 
 @Module({
   controllers: [ArticleController],
-  providers: [ArticleService],
+  providers: [ArticleService, DatabaseService],
 })
 export class ArticleModule {}
--- src/article/article.service.ts
+++ src/article/article.service.ts
@@ -1,26 +1,48 @@
 import { Injectable } from '@nestjs/common';
 import { CreateArticleDto } from './dto/create-article.dto';
 import { UpdateArticleDto } from './dto/update-article.dto';
+import { DatabaseService } from 'src/config/database/database.service';
+import { createChapters } from './utils/article.helpers';
+import { ArticleDto } from './dto/body';
+import { fullArticle } from './utils/article.query';
 
 @Injectable()
 export class ArticleService {
-  create(createArticleDto: CreateArticleDto) {
-    return 'This action adds a new article';
+  constructor(private readonly prisma: DatabaseService) {}
+
+  async create(createArticleDto: CreateArticleDto): Promise<ArticleDto> {
+    const { chapters } = createArticleDto;
+    const createdArticle = await this.prisma.article.create({
+      data: {
+        ...createArticleDto,
+        chapters: { create: createChapters(chapters) },
+      },
+    });
+    return new ArticleDto(createdArticle);
   }
 
-  findAll() {
-    return `This action returns all article`;
+  async findAll(): Promise<ArticleDto[]> {
+    const articles = await this.prisma.article.findMany();
+    return ArticleDto.fromEntities(articles);
   }
 
-  findOne(id: number) {
-    return `This action returns a #${id} article`;
+  async findOne(id: string): Promise<ArticleDto> {
+    const article = await this.prisma.article.findUniqueOrThrow({
+      where: { id },
+      include: fullArticle.include,
+    });
+    return new ArticleDto(article);
   }
 
-  update(id: number, updateArticleDto: UpdateArticleDto) {
-    return `This action updates a #${id} article`;
-  }
+  // async update(id: string, updateArticleDto: UpdateArticleDto): Promise<any> {
+  //   return this.prisma.article.update({
+  //     where: { id },
+  //     data: updateArticleDto,
+  //   });
+  // }
 
-  remove(id: number) {
-    return `This action removes a #${id} article`;
+  async remove(id: string): Promise<ArticleDto> {
+    const deletedArticle = await this.prisma.article.delete({ where: { id } });
+    return new ArticleDto(deletedArticle);
   }
 }
