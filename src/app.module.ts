a088f9a9be7d4a2ff456d877ce397a6162962b9c
Ltensio <76816528+Ltensio@users.noreply.github.com>
2025-03-05 16:39:28 +0100

    chore: fix env file path in app module, add authmodule to app module imports

--- src/app.module.ts
+++ src/app.module.ts
@@ -6,11 +6,13 @@ import { join } from 'path';
 import { ArticleModule } from './article/article.module';
 import { APP_GUARD } from '@nestjs/core';
 import { JwtGuard } from './shared/guards/jwt.guard';
+import { AuthModule } from './identity/auth/auth.module';
 
 @Module({
   imports: [
     ConfigModule.forRoot({ isGlobal: true, envFilePath: getEnvPath() }),
     ArticleModule,
+    AuthModule
   ],
   controllers: [AppController],
   providers: [AppService, { provide: APP_GUARD, useClass: JwtGuard }],
@@ -20,5 +22,5 @@ export class AppModule {}
 function getEnvPath(): string {
   const configService = new ConfigService();
   const env: string = configService.get<string>('NODE_ENV') || 'development';
-  return join(`src/config/env/${env}.env`);
+  return join(`.env.${env}`);
 }
