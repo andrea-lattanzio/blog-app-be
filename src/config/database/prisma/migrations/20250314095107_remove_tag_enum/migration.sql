e119460b16caa6aae4c5449583f3b404e925f309
Ltensio <76816528+Ltensio@users.noreply.github.com>
2025-03-14 10:55:14 +0100

    feat(schema): final article and user schema

---
+++ src/config/database/prisma/migrations/20250314095107_remove_tag_enum/migration.sql
@@ -0,0 +1,12 @@
+/*
+  Warnings:
+
+  - Changed the type of `tag` on the `Article` table. No cast exists, the column would be dropped and recreated, which cannot be done if there is data, since the column is required.
+
+*/
+-- AlterTable
+ALTER TABLE "Article" DROP COLUMN "tag",
+ADD COLUMN     "tag" TEXT NOT NULL;
+
+-- DropEnum
+DROP TYPE "ArticleTag";
