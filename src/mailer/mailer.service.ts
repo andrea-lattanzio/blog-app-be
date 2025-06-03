97374d13e6ab938d70f20f8f45647bd979101c18
Andrea Lattanzio <andrea.lattanzio@consulting4business.it>
2025-06-03 09:59:27 +0200

    add mail service

--- src/mailer/mailer.service.ts
+++ src/mailer/mailer.service.ts
@@ -1,9 +1,26 @@
 import { Injectable } from '@nestjs/common';
-import { MailerService } from "@nestjs-modules/mailer";
+import { MailerService } from '@nestjs-modules/mailer';
+import { join } from 'path';
 
 @Injectable()
 export class MailSenderService {
-    constructor(
-        private readonly mailerSrv: MailerService
-    ) {}
+  constructor(private readonly mailerSrv: MailerService) {}
+
+  send(email: string): void {
+    this.mailerSrv.sendMail({
+        from: "Andrea Lattanzio <andrealattanziodedona@gmail.com>",
+        // to: email,
+        to: "andrealattanziodedona@gmail.com",
+        subject: "Welcome to my newsletter!",
+        context: {
+            email: email
+        },
+        template: "welcome",
+        attachments: [{
+            filename: 'header-image.png',
+            path: join(__dirname, '..', '..', 'assets', 'header-image.png'),
+            cid: 'header-image',
+        }]
+    });
+  }
 }
