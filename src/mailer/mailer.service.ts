35bcb39b16d100229a7ec10f6eaa200bcd80222f
Andrea Lattanzio <andrea.lattanzio@consulting4business.it>
2025-06-03 11:53:32 +0200

    refactor email service to handle multiple users

--- src/mailer/mailer.service.ts
+++ src/mailer/mailer.service.ts
@@ -2,25 +2,44 @@ import { Injectable } from '@nestjs/common';
 import { MailerService } from '@nestjs-modules/mailer';
 import { join } from 'path';
 
+export interface MailOptions<TContext = any> {
+  subject: string;
+  context: TContext;
+  template: string;
+  attachments?: any[];
+}
+
+const ASSETS_BASE_PATH = join(__dirname, '..', '..', 'assets');
+
 @Injectable()
 export class MailSenderService {
   constructor(private readonly mailerSrv: MailerService) {}
 
-  send(email: string): void {
-    this.mailerSrv.sendMail({
-        from: "Andrea Lattanzio <andrealattanziodedona@gmail.com>",
-        // to: email,
-        to: "andrealattanziodedona@gmail.com",
-        subject: "Welcome to my newsletter!",
-        context: {
-            email: email
-        },
-        template: "welcome",
-        attachments: [{
-            filename: 'header-image.png',
-            path: join(__dirname, '..', '..', 'assets', 'header-image.png'),
-            cid: 'header-image',
-        }]
+  send<TContext>(
+    usersToNotify: string[],
+    options: MailOptions<TContext>,
+  ): void {
+    const defaultAttachments = [
+      {
+        filename: 'header-image.png',
+        path: join(ASSETS_BASE_PATH, 'header-image.png'),
+        cid: 'header-image',
+      },
+    ];
+
+    const attachments = options.attachments
+      ? [...defaultAttachments, ...options.attachments]
+      : defaultAttachments;
+
+    usersToNotify.forEach((email) => {
+      this.mailerSrv.sendMail({
+        from: 'Andrea Lattanzio <andrealattanziodedona@gmail.com>',
+        to: email,
+        subject: options.subject,
+        context: options.context,
+        template: options.template,
+        attachments: attachments,
+      });
     });
   }
 }
